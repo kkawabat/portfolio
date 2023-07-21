33386fd37b4f507018c539049fdd46ec43cbe577
kan kawabata <kkawabat@asu.edu>
2023-07-21 14:11:23 -0700

    added front end funcitonalities to magic eye page

--- magic_eye/urls.py
+++ magic_eye/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from magic_eye.views import magic_eye_view
+from magic_eye.views import magic_eye_view, magic_eye_generate_view, magic_eye_decode_view
 
 urlpatterns = [
     path('magic_eye', magic_eye_view, name='magic_eye'),
+    path('magic_eye_generate', magic_eye_generate_view),
+    path('magic_eye_decode', magic_eye_decode_view),
 ]
--- magic_eye/views.py
+++ magic_eye/views.py
@@ -1,5 +1,30 @@
+import base64
+
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def magic_eye_view(request):
     return render(request, 'magic_eye.html')
+
+
+def magic_eye_decode_view(request):
+    file = request.FILES.get('input_image')
+    if file is not None:
+        imgblob = base64.b64encode(file.read()).decode('utf-8')
+        img_uri = f'data:image/png;base64,{imgblob}'
+        return JsonResponse({'image_url': img_uri})
+    else:
+        return JsonResponse({'image_url': None})
+
+
+def magic_eye_generate_view(request):
+    t_file = request.FILES.get('texture')
+    d_file = request.FILES.get('depth_map')
+    if t_file is not None:
+        imgblob = base64.b64encode(t_file.read()).decode('utf-8')
+        img_uri = f'data:image/png;base64,{imgblob}'
+        return JsonResponse({'image_url': img_uri})
+    else:
+        return JsonResponse({'image_url': None})
+
