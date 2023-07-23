81460124e10d4c4da72263162466deff16a514a5
kan kawabata <kkawabat@asu.edu>
2023-07-22 17:25:44 -0700

    - updated projects and blogs section interaction behavior - added static_url to urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from django.urls import path, include
@@ -22,5 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('magic_eye.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
