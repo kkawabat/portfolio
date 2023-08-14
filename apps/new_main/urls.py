a6c8df85af768fc4588f64952f703e69fceda196
kan kawabata <kkawabat@asu.edu>
2023-08-13 22:09:28 -0700

    - continue working on new landing page (wip)

--- apps/new_main/urls.py
+++ apps/new_main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import main_view, cv_view, post_request_view, about_view, favicon
+from .views import main_view, cv_view, projects_view, about_view, favicon, blogs_view
 
 urlpatterns = [
     path('', main_view, name='new_main'),
     path('favicon.ico', favicon, name='favicon'),
-    path('cv', cv_view, name='cv'),
     path('about', about_view, name='about'),
-    path('post_request', post_request_view, name='post_request'),
+    path('projects', projects_view, name='projects'),
+    path('blogs', blogs_view, name='blogs'),
+    path('cv', cv_view, name='cv'),
 ]
--- apps/new_main/views.py
+++ apps/new_main/views.py
@@ -2,9 +2,31 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from apps.main.models import Post
+
+
+def favicon(_):
+    favicon_path = staticfiles_storage.path('images/favicon.png')
+    with open(favicon_path, 'rb') as favicon_img:
+        return HttpResponse(favicon_img.read(), content_type="image/png")
+
 
 def main_view(request):
     return render(request, 'new_main/index.html')
+
+
+def about_view(request):
+    return render(request, 'new_main/about.html')
+
+
+def projects_view(request):
+    project_list = Post.objects.filter(status=1, content_type=1).order_by('-created_on')
+    return render(request, 'new_main/projects.html', context={'projects': project_list})
+
+
+def blogs_view(request):
+    blog_list = Post.objects.filter(status=1, content_type=0).order_by('-created_on')
+    return render(request, 'new_main/blogs.html', context={'blogs': blog_list})
 
 
 def cv_view(_):
@@ -13,17 +35,3 @@ def cv_view(_):
         response = HttpResponse(pdf.read(), content_type='application/pdf')
         response['Content-Disposition'] = 'filename=KanKawabata_CV.pdf'
         return response
-
-
-def about_view(request):
-    return render(request, 'new_main/about.html')
-
-
-def post_request_view():
-    return None
-
-
-def favicon(_):
-    favicon_path = staticfiles_storage.path('images/favicon.png')
-    with open(favicon_path, 'rb') as favicon_img:
-        return HttpResponse(favicon_img.read(), content_type="image/png")
