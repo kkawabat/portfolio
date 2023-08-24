d59f152a6567d9e5ee76d8fdd6d2e86c9dab5174
kan kawabata <kkawabat@asu.edu>
2023-08-24 00:47:34 -0700

    - worked on chat highlight app    - added some business logic to fetch youtube chat logs    - added some modified code for generating horizon chart (wip)

--- apps/chat_highlights/urls.py
+++ apps/chat_highlights/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import details_view, app_view, index_view
-
+from .views import details_view, app_view, index_view, get_history_view
 urlpatterns = [
     path('', index_view, name='chat-highlight'),
     path('details', details_view, name='chat-highlight-details'),
-    path('app', app_view, name='chat-highlight-app')
+    path('app', app_view, name='chat-highlight-app'),
+    path('get_history', get_history_view, name='chat-highlight-get-history'),
+
 ]
--- apps/chat_highlights/views.py
+++ apps/chat_highlights/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+
+from apps.chat_highlights.chat_highlight_business_logic import parse_chat, fetch_youtube_chat_logs
 
 
 def index_view(request):
@@ -11,3 +14,12 @@ def details_view(request):
 
 def app_view(request):
     return render(request, "chat_highlights/index.html")
+
+
+def get_history_view(request):
+    youtube_url = request.POST['youtube-link']
+    youtube_url = 'https://www.youtube.com/watch?v=NrY0kCOc-zw&list=PLFs19LVskfNzQLZkGG_zf6yfYTp_3v_e6&ab_channel=Destiny'
+    chat_log_path = fetch_youtube_chat_logs(youtube_url)
+    highlight_data = parse_chat(chat_log_path)
+
+    return render(request, "chat_highlights/chat_highlight_chart.html", context={'data': highlight_data})
