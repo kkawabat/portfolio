01d801e380d034ae0a73a4bfffbd0e4eecd2c2f2
kan kawabata <kkawabat@asu.edu>
2024-06-16 09:42:26 -0700

    - added logging for debugging x2

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -161,7 +161,7 @@ LOGGING = {
         'file': {
             'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': 'debug.log',
+            'filename': os.path.join(BASE_DIR, 'debug.log'),
         },
     },
     'loggers': {
